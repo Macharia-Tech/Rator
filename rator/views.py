ac094a2039d24be02a1b4ff4b19c2e78a965dc86
AudreyCherrie <mitchelaudrey@gmail.com>
2020-02-17 17:24:29 +0300

    added rest_framework to provide users access to data from the application

--- rator/views.py
+++ rator/views.py
@@ -1,4 +1,4 @@
-rom django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
